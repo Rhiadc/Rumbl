78a77e5b78b514ce895cd2f59a5e6c98213cf950
Rhiad Ciccoli <rhiad.ciccoli@gmail.com>
2021-07-14 16:31:14 -0300

    add Auth plug to router pipeline

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -6,7 +6,8 @@ defmodule RumblWeb.Router do
     plug :fetch_session
     plug :fetch_flash
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug :put_secure_browser_header
+    plug RumblWeb.Auth
   end
 
   pipeline :api do
